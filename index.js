35c4ada029ec56046d98af122cd03c002a285e73
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-13 12:39:02 -0800

    Server boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send({hello: 'Afternoon Project!'});
+})
+
+const port = 5000;
+
+server.listen(port, () => console.log(`\n ** API on port ${port} ** \n`));
